fa3a98b46cafa6fe76bf4b8a27a65b4b2cf4bca0
panda939 <39067497+panda939@users.noreply.github.com>
2019-05-09 21:24:56 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+
+//панель элементов (админка)
+$(".c_menu").click(function() {
+	if ($(".g_menu").is(":visible")) {
+			$(".g_menu").hide();
+		}else{
+		$(".g_menu").show();
+		}
+		
+	
+	
+});
+
+
+});
